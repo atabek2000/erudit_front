fd885b71af395079e337632a9ea611d596a3fb37
atabek <nuriddin.atabek2000@gmail.com>
2025-09-03 12:07:04 +0500

    fix: scal

--- app/app.vue
+++ app/app.vue
@@ -12,7 +12,7 @@ useHead({
   meta: [
     {
       name: "viewport",
-      content: "width=device-width, initial-scale=1, maximum-scale=1",
+      content: "width=device-width, initial-scale=1, maximum-scale=1.0",
     },
     {
       name: "keywords",
